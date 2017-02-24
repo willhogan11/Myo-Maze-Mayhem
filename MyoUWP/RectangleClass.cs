e593388a3bd5a55e52094da327e5ac3de3e6377e
Will Hogan <willhogan11@hotmail.com>
2017-02-24 00:19:24 +0000

    Added RectangleClass. Working on Collision detection between multiple objects and ship

---
+++ MyoUWP/RectangleClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyoUWP
+{
+    public class RectangleClass
+    {
+        public float RadiusX { get; set; }
+        public float RadiusY { get; set; }
+        public float width { get; set; }
+        public float height { get; set; }
+    }
+}
--- MyoUWP/Test.xaml.cs
+++ MyoUWP/Test.xaml.cs
@@ -17,8 +17,9 @@ namespace MyoUWP
     /// </summary>
     public sealed partial class Test : Page
     {
-        Ellipse movableCircle;
         Canvas myCanvas;
+        Rectangle debris;
+        Rectangle ship;
 
         public Test()
         {
@@ -27,25 +28,23 @@ namespace MyoUWP
             Window.Current.CoreWindow.KeyDown += CoreWindow_KeyDown;
 
             // CreateCanvas();
-            // CreateShip();
+
         }
 
 
         // Example one Shape created on page load
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
+            // ellipseStoryBoard.Begin();
 
+            CreateShip();
 
-            ellipseStoryBoard.Begin();
+            int numberOfRectangles = 700;
 
-
-
-            // int numberOfRectangles = 800;
-
-            //for(int i = 0; i < numberOfRectangles; i++)
-            //{
-            //    CreateRectangle();
-            //}
+            for (int i = 0; i < numberOfRectangles; i++)
+            {
+                CreateRectangle();
+            }
         }
 
         private void CreateCanvas()
@@ -65,19 +64,19 @@ namespace MyoUWP
 
         private void CreateShip()
         {
-            SolidColorBrush blackBrush = new SolidColorBrush(Windows.UI.Colors.Black);
-
-            if (movableCircle == null)
+            if (ship == null)
             {
-                movableCircle = new Ellipse();
-                movableCircle.Width = 25;
-                movableCircle.Height = 25;
-                movableCircle.Fill = blackBrush;
+                SolidColorBrush blackBrush = new SolidColorBrush(Windows.UI.Colors.Black);
+                ship = new Rectangle();
+                ship.RadiusX = (float)Canvas.GetLeft(eMyo);
+                ship.RadiusY = (float)Canvas.GetTop(eMyo);
+                ship.Width = (float)eMyo.Width;
+                ship.Height = (float)eMyo.Height;
+                ship.Fill = blackBrush;
 
-                layoutRoot.Children.Add(movableCircle);
+                cvsRoller.Children.Add(ship);
             }
         }
-
 
 
         private void CreateEllipse()
@@ -103,28 +102,53 @@ namespace MyoUWP
         private void CreateRectangle()
         {
             Random random = new Random();
-            Rectangle rect;
+            debris = new Rectangle();
+            SolidColorBrush blueBrush = new SolidColorBrush(Windows.UI.Colors.Blue);
 
-            rect = new Rectangle();
-            rect.Width = random.Next(5, 100);
-            rect.Height = random.Next(5, 100);
+            debris.Width = 25;
+            debris.Height = 25;
 
             CompositeTransform transform = new CompositeTransform();
-            transform.TranslateX = random.Next(0, 720);
-            transform.TranslateY = random.Next(0, 800);
-            rect.RenderTransform = transform;
+            transform.TranslateX = random.Next(0, 975);
+            transform.TranslateY = random.Next(0, 525);
+            debris.RenderTransform = transform;
+            debris.Fill = blueBrush;
 
-            Color color = Color.FromArgb(255, (byte)random.Next(0, 255), (byte)random.Next(0, 255), (byte)random.Next(0, 255));
-            rect.Fill = new SolidColorBrush(color);
-
-            myCanvas.Children.Add(rect);
+            cvsRoller.Children.Add(debris);
         }
+
+
+
+
+        // Working Version of Rectangle Collision
+        #region
+        //private void CreateRectangle()
+        //{
+        //    Random random = new Random();
+        //    Rectangle rect;
+
+        //    rect = new Rectangle();
+        //    rect.Width = random.Next(5, 100);
+        //    rect.Height = random.Next(5, 100);
+
+        //    CompositeTransform transform = new CompositeTransform();
+        //    transform.TranslateX = random.Next(0, 920);
+        //    transform.TranslateY = random.Next(0, 800);
+        //    rect.RenderTransform = transform;
+
+        //    Color color = Color.FromArgb(255, (byte)random.Next(0, 255), (byte)random.Next(0, 255), (byte)random.Next(0, 255));
+        //    rect.Fill = new SolidColorBrush(color);
+
+        //    myCanvas.Children.Add(rect);
+        //}
+        #endregion
+
+
 
 
         // Get key press events working first or as a backup if myo isn't available or can't connect
         private void CoreWindow_KeyDown(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.KeyEventArgs args)
         {
-            
             if (args.VirtualKey == VirtualKey.Down)
             {
                 System.Diagnostics.Debug.WriteLine("Key Down Pressed");
@@ -151,84 +175,119 @@ namespace MyoUWP
 
         private void detectCollision(object sender, object e)
         {
-            SolidColorBrush redBrush = new SolidColorBrush(Windows.UI.Colors.Red);
-            SolidColorBrush whiteBrush = new SolidColorBrush(Windows.UI.Colors.White);
+            debris = new Rectangle();
+            debris.RadiusX = (float)Canvas.GetLeft(randomBlock);
+            debris.RadiusY = (float)Canvas.GetTop(randomBlock);
+            debris.Width = (float)randomBlock.Width;
+            debris.Height = (float)randomBlock.Height;
 
-            Rectangle rect1 = new Rectangle();
-            rect1.RadiusX = (float)Canvas.GetLeft(eMyo);
-            rect1.RadiusY = (float)Canvas.GetTop(eMyo);
-            rect1.Width = (float)eMyo.Width;
-            rect1.Height = (float)eMyo.Height;
-
-            Rectangle rect2 = new Rectangle();
-            rect2.RadiusX = (float)Canvas.GetLeft(blockObject);
-            rect2.RadiusY = (float)Canvas.GetTop(blockObject);
-            rect2.Width = (float)blockObject.Width;
-            rect2.Height = (float)blockObject.Height;
+            ship = new Rectangle();
+            ship.RadiusX = (float)Canvas.GetLeft(eMyo);
+            ship.RadiusY = (float)Canvas.GetTop(eMyo);
+            ship.Width = (float)eMyo.Width;
+            ship.Height = (float)eMyo.Height;
 
 
-            if ((rect1.RadiusX + rect1.Width >= rect2.RadiusX) &&
-                (rect1.RadiusX <= rect2.RadiusX + rect2.Width) &&
-                (rect1.RadiusY + rect1.Height >= rect2.RadiusY) &&
-                (rect1.RadiusY <= rect2.RadiusY + rect2.Height))
+            if ((ship.RadiusX + ship.Width >= debris.RadiusX) &&
+                (ship.RadiusX <= debris.RadiusX + debris.Width) &&
+                (ship.RadiusY + ship.Height >= debris.RadiusY) &&
+                (ship.RadiusY <= debris.RadiusY + debris.Height))
             {
-                Debug.WriteLine("Collision Detected...");
-                winGame.Visibility = Visibility.Visible;
-                cvsRoller.Background = whiteBrush;
-                gameText.Text = ("YOUR CRASHED!");
-                eMyo.Fill = redBrush;
+                Debug.WriteLine("Collision Detected");
             }
-            else
-            {
-                eMyo.Fill = whiteBrush;
-            }
-
-
-            
-
-
-            if (rect1.RadiusX >= 930 && rect1.RadiusY <= 34)
-            {
-                Debug.WriteLine("YOU REACHED THE ESCAPE POD!");
-                winGame.Visibility = Visibility.Visible;
-                cvsRoller.Background = whiteBrush;
-            }
-
-            
-
-
-            // Do something when Controlled Object is at the Canvas Edge
-            // Need to Redraw the Rectangle at Edge
-            if (rect1.RadiusX <= 0)
-            {
-                Debug.WriteLine("At Left Edge of Canvas");
-                eMyo.SetValue(Canvas.LeftProperty, (double)eMyo.GetValue(Canvas.LeftProperty) + 2);
-            }
-            if (rect1.RadiusY <= 0)
-            {
-                Debug.WriteLine("At Top Edge of Canvas");
-                eMyo.SetValue(Canvas.TopProperty, (double)eMyo.GetValue(Canvas.TopProperty) + 2);
-            }
-            if(rect1.RadiusX + eMyo.Width >= cvsRoller.Width)
-            {
-                Debug.WriteLine("At Right Edge of Canvas");
-                eMyo.SetValue(Canvas.LeftProperty, (double)eMyo.GetValue(Canvas.LeftProperty) - 2);
-            }
-            if (rect1.RadiusY + eMyo.Height >= cvsRoller.Height)
-            {
-                Debug.WriteLine("At Bottom Edge of Canvas");
-                eMyo.SetValue(Canvas.TopProperty, (double)eMyo.GetValue(Canvas.TopProperty) - 2);
-            }
-
-
-            //if(MyAnimatedEllipseGeometry.RadiusX > cvsRoller.Height)
-            //{
-            //    Debug.WriteLine("Ball hit canvas edge....");
-            //}
-
-
-            // Update the Display Text boxes on screen with X Y Coordinates
-            rect1X.Text = ("Rect 1 X : " + rect1.RadiusX.ToString()); 
         }
+
+
+
+
+        // Pass in a Rectangle object to check for Collisions.....
+        //private void detectCollision(object sender, object e, Rectangle rect)
+        //{
+
+        //    SolidColorBrush redBrush = new SolidColorBrush(Windows.UI.Colors.Red);
+        //    SolidColorBrush whiteBrush = new SolidColorBrush(Windows.UI.Colors.White);
+
+        //    Rectangle rect1 = new Rectangle();
+        //    rect1.RadiusX = (float)Canvas.GetLeft(eMyo);
+        //    rect1.RadiusY = (float)Canvas.GetTop(eMyo);
+        //    rect1.Width = (float)eMyo.Width;
+        //    rect1.Height = (float)eMyo.Height;
+
+        //    Rectangle rect2 = new Rectangle();
+        //    rect2.RadiusX = (float)Canvas.GetLeft(blockObject);
+        //    rect2.RadiusY = (float)Canvas.GetTop(blockObject);
+        //    rect2.Width = (float)blockObject.Width;
+        //    rect2.Height = (float)blockObject.Height;
+
+
+        //    if ((rect1.RadiusX + rect1.Width >= rect2.RadiusX) &&
+        //        (rect1.RadiusX <= rect2.RadiusX + rect2.Width) &&
+        //        (rect1.RadiusY + rect1.Height >= rect2.RadiusY) &&
+        //        (rect1.RadiusY <= rect2.RadiusY + rect2.Height))
+        //    {
+        //        Debug.WriteLine("Collision Detected...");
+        //        winGame.Visibility = Visibility.Visible;
+        //        cvsRoller.Background = whiteBrush;
+        //        gameText.Text = ("YOU CRASHED!");
+        //        // eMyo.Fill = redBrush;
+        //    }
+        //    else
+        //    {
+        //        eMyo.Fill = whiteBrush;
+        //    }
+
+
+
+        //    // **********************************************************************8
+        //    // NOTE: Will need to refactor Rectangle into a separate class....
+        //    // RectangleClass rectClass = new RectangleClass();
+        //    // rectClass.RadiusX = (float)Canvas.GetLeft(eMyo);
+        //    // **********************************************************************8
+
+
+
+        //    if (rect1.RadiusX >= 930 && rect1.RadiusY <= 34)
+        //    {
+        //        winGame.Visibility = Visibility.Visible;
+        //        cvsRoller.Background = whiteBrush;
+        //    }
+
+
+
+
+        //    // Do something when Controlled Object is at the Canvas Edge
+        //    // Need to Redraw the Rectangle at Edge
+        //    if (rect1.RadiusX <= 0)
+        //    {
+        //        // Debug.WriteLine("At Left Edge of Canvas");
+        //        eMyo.SetValue(Canvas.LeftProperty, (double)eMyo.GetValue(Canvas.LeftProperty) + 2);
+        //    }
+        //    if (rect1.RadiusY <= 0)
+        //    {
+        //        // Debug.WriteLine("At Top Edge of Canvas");
+        //        eMyo.SetValue(Canvas.TopProperty, (double)eMyo.GetValue(Canvas.TopProperty) + 2);
+        //    }
+        //    if(rect1.RadiusX + eMyo.Width >= cvsRoller.Width)
+        //    {
+        //        // Debug.WriteLine("At Right Edge of Canvas");
+        //        eMyo.SetValue(Canvas.LeftProperty, (double)eMyo.GetValue(Canvas.LeftProperty) - 2);
+        //    }
+        //    if (rect1.RadiusY + eMyo.Height >= cvsRoller.Height)
+        //    {
+        //        // Debug.WriteLine("At Bottom Edge of Canvas");
+        //        eMyo.SetValue(Canvas.TopProperty, (double)eMyo.GetValue(Canvas.TopProperty) - 2);
+        //    }
+
+
+        //    //if(MyAnimatedEllipseGeometry.RadiusX > cvsRoller.Height)
+        //    //{
+        //    //    Debug.WriteLine("Ball hit canvas edge....");
+        //    //}
+
+
+        //    // Update the Display Text boxes on screen with X Y Coordinates
+        //    rect1X.Text = ("Rect 1 X : " + rect1.RadiusX.ToString()); 
+        //}
+
     }
 }
